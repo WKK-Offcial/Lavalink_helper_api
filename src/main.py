8f7970d6331d768c675206182c11db3dd377150e
RakuzanTheBoy <xadix222@gmail.com>
2023-03-20 18:16:48 +0100

    Implemented saving file

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 from dropbox_storage import DropboxManager
+from pydantic import BaseModel
+from pathlib import Path
 import os
 import uvicorn
+import base64
+
 
 load_dotenv()
 app = FastAPI()
 
 dropbox = DropboxManager()
 dropbox.download_all()
+
+class SoundModel(BaseModel):
+    file_data: str
+    file_name: str
 
 @app.get('/')
 async def test():
@@ -21,13 +29,20 @@ async def get_sounds(guild_id: str):
         files.sort()
         ret = {"files": files}
     except:
-        raise HTTPException(status_code=404, detail="Wrong Guild Id")
+        raise HTTPException(status_code=418, detail="Wrong Guild Id")
     return ret
 
 @app.post('/sounds/{guild_id}')
-async def save_sound(guild_id: str):
-    # TODO: YES
-    return {"message": "save sound"}
+async def save_sound(guild_id: str, body: SoundModel):
+    Path(f'./sounds/{guild_id}').mkdir(parents=True, exist_ok=True)
+    print(body.file_name)
+    if not body.file_name.endswith('.mp3'):
+        raise HTTPException(status_code=418, detail="fsdfsdffs The fuck is this!? .mp3 only")
+    file_data = base64.b64decode(body.file_data)
+    with open(f'./sounds/{guild_id}/{body.file_name}', "wb") as f: # write binary to file
+        f.write(file_data)
+    dropbox.upload_file(f'./sounds/{guild_id}/{body.file_name}', f'{guild_id}')
+    return {"message": "everything daijoubu"}
     
 if __name__ == "__main__":
     load_dotenv()
@@ -35,38 +50,3 @@ if __name__ == "__main__":
     dropbox.download_all()
     
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-#     """ Simple API module """
-# import json
-# import base64
-# import falcon
-
-# class Endpoints:
-#     """
-#     Class with endpoints required by discord bot
-#     """
-#     def on_get(self, req, res):
-#         """
-#         Handle GET requests.
-#         """
-#         test_dict = {'203114439468253184':['1','2', '3'], '1084958968302022805':['5','2', '4']}
-#         res.text = json.dumps(test_dict) # Dictionary to json
-
-#     def on_post(self, req, res):
-#         """
-#         Handle POST requests.
-#         """
-#         try:
-#             mp3_data = req.get_media().get('mp3') # Get mp3 ecoded in base64 from json
-#             guild_id = req.get_media().get('guild_id') # Get guild id
-#             file_data = base64.b64decode(mp3_data) # decode b64 to binary
-#             with open('aud.mp3', "wb") as file: # write binary to file
-#                 file.write(file_data)
-#             res.text = json.dumps({"message":"Done!"})
-#         except Exception:
-#             res.text = json.dumps({"message":"Failed!"})
-#             res.status = falcon.HTTP_500
-
-# app = falcon.App()
-# app.add_route('/sounds', Endpoints())
