540c0505f60818df22f9bb1bbbaf6ee13401bebc
RakuzanTheBoy <xadix222@gmail.com>
2023-03-21 21:13:42 +0100

    Adding title to metadata on save

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from dropbox_storage import DropboxManager
 from pydantic import BaseModel
 from pathlib import Path
+from mutagen.mp3 import EasyMP3
 import os
 import uvicorn
 import base64
@@ -15,8 +16,8 @@ dropbox = DropboxManager()
 dropbox.download_all()
 
 class SoundModel(BaseModel):
-    file_data: str
     file_name: str
+    file_data: str
 
 @app.get('/')
 async def test():
@@ -35,14 +36,17 @@ async def get_sounds(guild_id: str):
 @app.post('/sounds/{guild_id}')
 async def save_sound(guild_id: str, body: SoundModel):
     Path(f'./sounds/{guild_id}').mkdir(parents=True, exist_ok=True)
-    print(body.file_name)
     if not body.file_name.endswith('.mp3'):
         raise HTTPException(status_code=418, detail="fsdfsdffs The fuck is this!? .mp3 only")
     body.file_name = body.file_name.lower()
     file_data = base64.b64decode(body.file_data)
-    with open(f'./sounds/{guild_id}/{body.file_name}', "wb") as f: # write binary to file
+    file_path = f'./sounds/{guild_id}/{body.file_name}'
+    with open(file_path, "wb") as f: # write binary to file
         f.write(file_data)
-    dropbox.upload_file(f'./sounds/{guild_id}/{body.file_name}', f'{guild_id}')
+    tags=EasyMP3(file_path)
+    tags['title'] = body.file_name.replace('_', ' ').capitalize().split('.mp3')[0]
+    tags.save()
+    dropbox.upload_file(file_path, f'{guild_id}')
     return {"message": "everything daijoubu"}
     
 if __name__ == "__main__":
